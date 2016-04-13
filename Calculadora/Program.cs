2147b5728fac80f52037f638f1c78bc89a79a418
antonioljunior <antonioljunior@gmail.com>
2016-04-13 19:27:10 -0300

    Start Commit

---
+++ Calculadora/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero1, numero2;
+            string msgErro = "O valor informado não é um numero válido.";
+
+            Console.WriteLine("Numero 1: ");
+
+            while (!double.TryParse(Console.ReadLine(), out numero1))
+                Console.WriteLine(msgErro);
+
+            Console.WriteLine("Numero 2: ");
+            while (!double.TryParse(Console.ReadLine(), out numero2))
+                Console.WriteLine(msgErro);
+
+            Console.WriteLine("Operação: + - * / ");
+            string operacao = Console.ReadLine();
+
+            switch (operacao)
+            {
+                case "+":
+                    Console.WriteLine(string.Format("Soma: {0}",
+                        numero1 + numero2));
+                    break;
+                case "-":
+                    Console.WriteLine(string.Format("Subtração: {0}",
+                        numero1 - numero2));
+                    break;
+                default:
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
